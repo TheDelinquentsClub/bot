e4873ffa9080b655c7543f1db8e516236cf0cb0e
Sepruko <natsepruko@gmail.com>
2022-05-23 18:04:40 +1000

    feat(main,mod): Update Go to 1.18, refactor and update dependencies, rewrite main.go

--- go.mod
+++ go.mod
@@ -1,22 +1,27 @@
-module github.com/kingultron99/tdcbot
+module github.com/TheDelinquentsClub/bot
 
-go 1.16
+go 1.18
 
 require (
 	github.com/Krognol/go-wolfram v0.0.0-20180610151123-5b91101b92a8
-	github.com/diamondburned/arikawa v1.3.14 // indirect
-	github.com/diamondburned/arikawa/v3 v3.0.0-rc.2 // direct
-	github.com/gin-gonic/gin v1.7.7 // indirect
-	github.com/googollee/go-socket.io v1.6.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/diamondburned/arikawa/v3 v3.0.0-rc.6
+	github.com/googollee/go-socket.io v1.6.1
+	github.com/lukasl-dev/waterlink v1.1.3
+	github.com/pkg/errors v0.9.1
+	go.uber.org/zap v1.19.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/lukasl-dev/waterlink v1.1.3 // indirect
-	github.com/mattn/go-colorable v0.1.11 // indirect
-	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
-	github.com/socketio/socket.io v0.0.0-20220423224557-8ecfcba5c14c // indirect
-	github.com/zhouhui8915/engine.io-go v0.0.0-20150910083302-02ea08f0971f // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
-	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,80 +1,123 @@
 package main
 
 import (
+	"bufio"
 	"context"
-	"fmt"
+	"github.com/TheDelinquentsClub/bot/util"
+	"github.com/diamondburned/arikawa/v3/api"
 	"github.com/diamondburned/arikawa/v3/discord"
 	"github.com/diamondburned/arikawa/v3/gateway"
-	"github.com/diamondburned/arikawa/v3/gateway/shard"
+	"github.com/diamondburned/arikawa/v3/session/shard"
 	"github.com/diamondburned/arikawa/v3/state"
-	"github.com/kingultron99/tdcbot/commands"
-	"github.com/kingultron99/tdcbot/core"
-	"github.com/kingultron99/tdcbot/logger"
-	"github.com/kingultron99/tdcbot/utils"
-	"github.com/kingultron99/tdcbot/websockets"
+	"go.uber.org/zap"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+func init() {
+	zap.ReplaceGlobals(zap.New(util.LoggerCore))
+}
+
 func main() {
-
-	core.Initialise()
-
-	newShard := state.NewShardFunc(func(m *shard.Manager, s *state.State) {
-		// Add the needed Gateway intents.
-		s.AddIntents(gateway.IntentGuilds)
-		s.AddIntents(gateway.IntentGuildMessages)
-		s.AddIntents(gateway.IntentDirectMessages)
-		s.AddIntents(gateway.IntentGuildVoiceStates)
-		core.State = s
-	})
-
-	m, err := shard.NewManager(fmt.Sprint("Bot ", core.Config.Token), newShard)
+	logger := zap.S()
+	config, err := util.NewConfig("config.json")
 	if err != nil {
-		logger.Error(fmt.Sprintf("failed to create shard manager: %v", err))
+		logger.Fatal("Failed to parse configuration file:", err.Error())
 	}
 
-	if err := m.Open(context.Background()); err != nil {
-		logger.Error(fmt.Sprintf("failed to connect shards: %v", err))
-	}
-	defer func(m *shard.Manager) {
-		err := m.Close()
+	manager, err := shard.NewManager(config.Token, state.NewShardFunc(func(m *shard.Manager, s *state.State) {
+		self, err := s.Me()
 		if err != nil {
-		}
-	}(m)
-
-	var shardNum int
-
-	m.ForEach(func(s shard.Shard) {
-		botState := s.(*state.State)
-
-		u, err := botState.Me()
-		if err != nil {
-			logger.Error(fmt.Sprintf("failed to get myself: %v", err))
+			m.ForEach(func(shard shard.Shard) {
+				_ = shard.Close()
+			})
+			log.Fatalf("Failed to get own user for shard #%d: %s", m.NumShards(), err.Error())
 		}
 
-		logger.Info(fmt.Sprintf("Shard %d/%d started as %s", shardNum, m.NumShards()-1, u.Tag()))
-
-		shardNum++
-	})
-
-	core.State.AddHandler(func(update *gateway.VoiceServerUpdateEvent) {
-		logger.Debug(update.Endpoint)
-		core.Update = update
-	})
-	core.State.AddHandler(func(c *gateway.MessageCreateEvent) {
-		if core.IsServerConnected {
-			if c.Message.ChannelID.String() == core.Config.BridgeChannelID {
-				if c.Author.Bot == false {
-					core.WSServer.BroadcastToNamespace("/", "discordmessage", c.Message.Content, c.Member.User.Username)
+		s.AddHandler(func(msg *gateway.MessageCreateEvent) {
+			// Simply checks only this for now, for testing.
+			if msg.ChannelID == config.MinecraftChannelID {
+				if msg.Author.Bot || msg.Author.Discriminator == "0000" {
+					return
 				}
+				_, _ = s.SendMessageComplex(msg.ChannelID, api.SendMessageData{
+					Embeds: []discord.Embed{
+						{
+							Author: &discord.EmbedAuthor{
+								Name: msg.Author.Username,
+								Icon: msg.Author.AvatarURL(),
+							},
+							Description: msg.Content,
+							Timestamp:   discord.NowTimestamp(),
+						},
+					},
+				})
+			}
+		})
+
+		s.AddHandler(func(interaction *gateway.InteractionCreateEvent) {
+			// Nothing to do, yet.
+		})
+
+		s.AddIntents(
+			gateway.IntentGuilds |
+				gateway.IntentGuildMessages |
+				gateway.IntentDirectMessages |
+				gateway.IntentGuildVoiceStates,
+		)
+
+		if err := s.Open(context.Background()); err != nil {
+			m.ForEach(func(shard shard.Shard) {
+				_ = shard.Close()
+			})
+			log.Fatalf("Failed to start Gateway connection for shard %d: %s", m.NumShards(), err.Error())
+		}
+
+		logger.Infof("Started shard #%d as %s", m.NumShards(), self.Tag())
+	}))
+	if err != nil {
+		logger.Fatal("Failed to create a shard manager:", err.Error())
+	}
+
+	inputReader := bufio.NewReader(os.Stdin)
+	killCh := make(chan os.Signal, 1)
+	signal.Notify(killCh, os.Interrupt, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-killCh:
+			logger.Info("Are you sure you want to terminate? (Y/N)")
+			text := func() string {
+				text, _ := inputReader.ReadString('\n')
+
+				l := len(text) - 1
+				if l == -1 || l == 0 {
+					return ""
+				}
+				if text[l-1] == '\r' {
+					l -= 1
+				}
+
+				return text[:l]
+			}()
+
+			switch text {
+			case "Y", "y", "Yes", "yes":
+				logger.Info("Shutting down process...")
+				manager.ForEach(func(shard shard.Shard) {
+					_ = shard.Close()
+				})
+				_ = util.LoggerCore.Sync()
+				os.Exit(0)
+			case "N", "n", "No", "no":
+				logger.Info("Continuing...")
+				continue
+			default:
+				logger.Error("I didn't understand your input, continuing.")
+				continue
 			}
 		}
-
-	})
-	commands.AddHandlers()
-	commands.Register(discord.AppID(utils.MustSnowflakeEnv(core.Config.APPID)), discord.GuildID(utils.MustSnowflakeEnv(core.Config.GUILDID)))
-
-	go websockets.InitServer()
-	//block forever
-	select {}
-
+	}
 }
